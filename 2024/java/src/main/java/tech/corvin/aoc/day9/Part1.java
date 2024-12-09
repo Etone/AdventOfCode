5ba814cd39820da404b0a5cea5397180f3f1cd16
Corvin Schapoehler <co.schapoehler@gmail.com>
2024-12-09 13:35:15 +0100

    Optimize Day 9

---
+++ 2024/java/src/main/java/tech/corvin/aoc/day9/Part1.java
@@ -0,0 +1,77 @@
+package tech.corvin.aoc.day9;
+
+import tech.corvin.aoc.general.Helper;
+import tech.corvin.aoc.general.Part;
+
+import java.io.IOException;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class Part1 implements Part<Long> {
+
+    private static final Integer FREE_DISK = -1;
+
+    @Override
+    public Long solve() throws IOException {
+        var diskmap = Helper.getResourceFileAsString("day9.txt");
+
+        var uncompressedDisk = buildDisk(diskmap);
+        var compressedDisk = compressDisk(uncompressedDisk);
+
+        return checksum(compressedDisk);
+    }
+
+
+
+    private List<Integer> buildDisk(String diskmap) {
+        var disk = new ArrayList<Integer>();
+
+        var chars = diskmap.split("");
+        var fileId = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            var count = Integer.parseInt(chars[i]);
+
+            if (i % 2 == 0) {
+                disk.addAll(Collections.nCopies(count, fileId));
+                fileId++;
+            } else {
+                disk.addAll(Collections.nCopies(count, FREE_DISK));
+            }
+        }
+
+        return disk;
+    }
+
+
+    private List<Integer> compressDisk(List<Integer> uncompressedDisk) {
+        var compressed = new ArrayList<>(uncompressedDisk);
+
+        var freeSpaceId = uncompressedDisk.indexOf(FREE_DISK);
+
+        for (var i = uncompressedDisk.size() - 1; i >= 0; i--) {
+            var fileId = uncompressedDisk.get(i);
+            if (fileId.equals(FREE_DISK)) continue;
+
+            compressed.set(i, FREE_DISK);
+            freeSpaceId = compressed.indexOf(FREE_DISK);
+            compressed.set(freeSpaceId, fileId);
+        }
+
+        return compressed;
+    }
+
+    private long checksum(List<Integer> disk) {
+        long result = 0;
+        for (int i = 0; i < disk.size(); i++) {
+            if (disk.get(i) == -1) continue;
+            result += (long) i * disk.get(i);
+        }
+
+        return result;
+    }
+}
