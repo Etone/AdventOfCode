d65da5519636161a46c315ecdb9ac8340b652d24
Corvin Schapoehler <co.schapoehler@gmail.com>
2024-12-15 00:42:28 +0100

    Refactor Day6

--- 2024/java/src/main/java/tech/corvin/aoc/year2024/Run2024.java
+++ 2024/java/src/main/java/tech/corvin/aoc/year2024/Run2024.java
@@ -15,7 +15,8 @@ public class Run2024 {
     static {
         try {
             days = List.of(
-                    new Day1(), new Day2(), new Day3(), new Day4(), new Day5()
+                    new Day1(), new Day2(), new Day3(), new Day4(), new Day5(),
+                    new Day6()
             );
         } catch (IOException e) {
             throw new RuntimeException(e);
--- 2024/java/src/main/java/tech/corvin/aoc/year2024/day6/PathStep.java
+++
@@ -1,9 +0,0 @@
-package tech.corvin.aoc.year2024.day6;
-
-import tech.corvin.aoc.general.grid.Coordinate;
-
-public record PathStep(
-        Coordinate coordinate,
-        Coordinate direction
-) {
-}
